9facc7d300cce15db53450cc10e152657d1920b9
Angus McAloon <AMcAloon@lcs.on.ca>
2020-11-26 08:56:51 -0500

    Initial Commit

---
+++ TheGoodDoggo/TheGoodDoggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGoodDoggoApp.swift
+//  TheGoodDoggo
+//
+//  Created by Angus McAloon on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct TheGoodDoggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
